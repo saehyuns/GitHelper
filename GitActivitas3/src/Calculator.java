287a6cb3984624907ea2c57dd339ffe8ab2d65f1
Sae Hyun Song <17700563+saehyuns@users.noreply.github.com>
2018-10-25 15:40:26 -1000

    Created Activity 3

---
+++ GitActivitas3/src/Calculator.java
@@ -0,0 +1,16 @@
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+
+	// Person 1: Create a method to add two numbers
+	
+	// Person 2: Create a method to subtract two numbers
+	
+	// Person 3: Create a method to multiply two numbers
+	
+	// Person 4: Create a method to divide two numbers
+	
+}
